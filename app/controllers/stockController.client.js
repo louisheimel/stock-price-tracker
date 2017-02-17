a6b8a4704a2d83cd8b6224a7a6795d2f8fb75308
Unknown Name <louis.heimel@mines.sdsmt.edu>
2017-02-17 20:29:42 +0000

    fix get_all_stocks route

--- app/controllers/stockController.client.js
+++ app/controllers/stockController.client.js
@@ -1,15 +1,57 @@
 'use strict';
-
+var something;
 (function() {
     var form = document.getElementsByTagName('form')[0];
+    
+    google.charts.load('current', {packages: ['corechart', 'line']});
+    google.charts.setOnLoadCallback(drawBasic);
+    
+    renderAllStocks();
     
     form.addEventListener('submit', function(e) {
       e.preventDefault();
       var value = e.target[0].value;
       // get stock data here
       ajaxFunctions.ajaxRequest('GET', '/get_stock?stock=' + value, function(data) {
-        console.log(data)
+        var div = document.querySelector('div.container');
+        
+        var newDiv = document.createElement('div');
+        var para = document.createElement('p').appendChild(document.createTextNode(value.toUpperCase()));
+        newDiv.appendChild(para)
+        div.appendChild(newDiv);
       })
       
-    })
+    });
+
+    function renderAllStocks() {
+      ajaxFunctions.ajaxRequest('GET', '/get_all_stocks', function(stocks) {
+        console.log('hello, here are teh stocks ' + stocks);
+        
+        stocks.forEach((stock) => {
+          console.log(stock);
+        })
+      })
+    }
+    
+    function drawBasic(stockData) {
+          console.log(stockData)
+          var data = new google.visualization.DataTable();
+          data.addColumn('date', 'Y');
+          data.addColumn('number', 'X');
+    
+          data.addRows(stockData.data);
+    
+          var options = {
+            hAxis: {
+              title: 'Time'
+            },
+            vAxis: {
+              title: 'Price'
+            }
+          };
+    
+          var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+    
+          chart.draw(data, options);
+        }
 })();
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,7 @@ var path = process.cwd();
 var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 // var StockHandler = require(path + '/app/controllers/stockHandler.server.js');
 var googleFinance = require('google-finance');
+var Stock = require('../models/stocks.js');
 
 
 module.exports = function (app, passport) {
@@ -24,7 +25,25 @@ module.exports = function (app, passport) {
 			    to: '2014-12-31',
 			}, function(err, data) {
 			    if (err) throw err;
+			    Stock.findOne({symbol: req.query.stock.toUpperCase()}, function(err, stock) {
+			    	if (err) throw err;
+			    	if (!stock) {
+			    		var stock = new Stock({
+					    	symbol: req.query.stock.toUpperCase(),
+					    	data: data,
+					    });
+					    stock.save();
+			    	}
+			    })
+
 			    res.json(data);
 			});
+		});
+		
+	app.route('/get_all_stocks')
+		.get(function(req, res, next) {
+			Stock.find({}, function(err, stocks) {
+				res.json(stocks);
+			})
 		})
 };
